04f02418b674c75cedceeb3e8996f33b886b7142
alex <mike123777mike@gmail.com>
2023-03-04 21:30:05 +0300

    release2

--- scripts/search.js
+++ scripts/search.js
@@ -16,14 +16,14 @@ for (let row in data) {
 let input = document.getElementById("search");
 let list = document.getElementById("list");
 input.oninput = function() {
-    let str = input.value;
+    let str = input.value.toLowerCase();
     list.innerHTML = "";
     if (str.length < 1)  // здесь можно изменить количество символов, при котором будет срабатывать поиск
         return;
+    str = str.replaceAll('0','⁰').replaceAll('1','¹').replaceAll('2','²').replaceAll('3','³').replaceAll('4','⁴')
+    .replaceAll('5','⁵').replaceAll('6','⁶').replaceAll('7','⁷').replaceAll('8','⁸').replaceAll('9','⁹').replaceAll('-','⁻')
     for (let name in dict) {
-        str = str.replaceAll('0','⁰').replaceAll('1','¹').replaceAll('2','²').replaceAll('3','³').replaceAll('4','⁴')
-        .replaceAll('5','⁵').replaceAll('6','⁶').replaceAll('7','⁷').replaceAll('8','⁸').replaceAll('9','⁹').replaceAll('-','⁻')
-        if (name.toLowerCase().indexOf(str.toLowerCase()) != -1) {
+        if (name.toLowerCase().indexOf(str) != -1) {
             let li = document.createElement('li');
             li.innerHTML = "<span style=\"margin-right: auto;\">" + name + "</span><span>" + dict[name] + "</span>";
             list.append(li);
